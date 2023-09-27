0f47fb81154467c5aaa265e6cc8d511204f54594
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-09-27 23:16:14 +0900

    Tugatim: bacic, languages

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 console.log("Web Serverni  boshlash");
 const express = require("express");
-
 const app = express();
 const http = require("http");
+const fs = require("fs");
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 //1KIRISH CODE
 
@@ -18,8 +26,11 @@ app.set("view engine", "ejs");
 
 //4 Routing code
 app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
+  //console.log(req.body);
+  //res.json({ test: "success" });
+});
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
